19952757351ccdf1970fdf7daaeb2e9dec25fdc9
Sabrina Maia <sabrinamaia-dev@gmail.com>
2026-08-26 15:12:02 -0300

    Adiciona mensagem para categoria sem noticias

--- app.py
+++ app.py
@@ -11,9 +11,13 @@ noticias = [
     }
 ]
 categoria_escolhida = input("Digite a categoria desejada: ")
+noticia_encontrada = False
 for noticia in noticias:
     if noticia["categoria"].lower() == categoria_escolhida.lower():
+        noticia_encontrada = True
         print("Título:", noticia["titulo"])
         print("Fonte:", noticia["fonte"])
         print("Categoria:", noticia["categoria"])
         print()
+if not noticia_encontrada:
+    print("Nenhuma notícia encontrada para a categoria selecionada.")
